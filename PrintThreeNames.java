0d8a9ba1d031c0da4ff08c4003d223dc926f550c
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-10-08 14:16:05 +0530

    [Ajay] ADD: Write a java code print three name in reverse

---
+++ PrintThreeNames.java
@@ -0,0 +1,14 @@
+        import java.util.*;
+           class PrintThreeNames{
+               public static void main(String [] args){
+                      Scanner sc = new Scanner(System.in);
+                      System.out.print("Enter first names :");
+                      String name1 = sc.nextLine();
+                      System.out.print("Enter second names :");
+                      String name2 = sc.nextLine();
+                      System.out.print("Enter third names :");
+                      String name3 = sc.nextLine();
+                      
+                      System.out.println("Hi" +  " " + name3+ " " + name2 + " "  + name1 + " ");
+                     }
+                  }
