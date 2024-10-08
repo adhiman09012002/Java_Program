7e0a10e88ff534d1d55301fc04abe7907eeb76cb
Ajay Dhiman <ajaydhiman09012002@gmail.com>
2024-10-08 14:55:14 +0530

    [Ajay] ADD: Write a java code print five place name using comma separated

---
+++ PrintFivePlace.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class PrintFivePlace{
+      public static void main(String [] args){
+             Scanner sc = new Scanner(System.in);
+             System.out.print("Enter first place :");
+             String name1 = sc.nextLine();
+             System.out.print("Enter second place :");
+             String name2 = sc.nextLine();
+             System.out.print("Enter third place :");
+             String name3 = sc.nextLine();
+             System.out.print("Enter fourth place :");
+             String name4 = sc.nextLine();
+             System.out.print("Enter fifth place :");
+             String name5 = sc.nextLine();
+             System.out.println(name1 +","+ name2 +","  + name3 +"," + name4 +","+ name5);
+        }
+}
